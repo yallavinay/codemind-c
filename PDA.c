4acbd3d79cac84b1033fc7fcba5e1a19d080f8c9
yallavinay <119720076+yallavinay@users.noreply.github.com>
2022-12-08 20:56:56 +0530

    // perfect number #include<stdio.h> int main() {     int n,sum=0,i;     scanf("%d",&n);     for(i=1;i<n;i++)     {         if(n%i==0)         {             sum+=i;         }     }     if(sum==n)
        {
            printf("PERFECT");
        }
        else if(sum<=n)
        {
            printf("DEFICIENT");
        }
        else
        {
            printf("ABUNDANT");
        }
    }

---
+++ PDA.c
@@ -0,0 +1,27 @@
+// perfect number
+#include<stdio.h>
+int main()
+{
+    int n,sum=0,i;
+    scanf("%d",&n);
+    for(i=1;i<n;i++)
+    {
+        if(n%i==0)
+        {
+            sum+=i;
+        }
+    }
+    if(sum==n)
+    
+    {
+        printf("PERFECT");
+    }
+    else if(sum<=n)
+    {
+        printf("DEFICIENT");
+    }
+    else
+    {
+        printf("ABUNDANT");
+    }
+}
