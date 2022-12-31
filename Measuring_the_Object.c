f42948bb9f0169823e4bc539ed1f244d0e4ba46e
yallavinay <119720076+yallavinay@users.noreply.github.com>
2022-12-31 09:47:46 +0530

    #include<stdio.h> int main() {     int a,b,c,d;     scanf("%d%d%d%d",&a,&b,&c,&d);     if(a==b || a==c || a==d || a==b+c || a==b+d || a==c+d)     {         printf("YES");     }
        else
        {
            printf("NO");
        }
    }

---
+++ Measuring_the_Object.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    int a,b,c,d;
+    scanf("%d%d%d%d",&a,&b,&c,&d);
+    if(a==b || a==c || a==d || a==b+c || a==b+d || a==c+d)
+    {
+        printf("YES");
+    }
+    
+    else
+    {
+        printf("NO");
+    }
+}
