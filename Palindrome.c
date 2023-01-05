4c3c044f2c58cdadbe4083d2a2fac1d55e6dd550
yallavinay <119720076+yallavinay@users.noreply.github.com>
2023-01-05 10:41:25 +0530

    // plindrome #include<stdio.h> int main() {     int n,s=0,q,r,rem;     scanf("%d",&n);     q=n;     while(q!=0)     {         r=q%10;         s=s*10+r;         q=q/10;     }     if(s==n)     {         printf("Palindrome");     }     else     {         printf("Not Palindrome");     } }

--- Palindrome.c
+++ Palindrome.c
@@ -1,21 +1,22 @@
+// plindrome
 #include<stdio.h>
 int main()
 {
-    int n,q,r,s=0;
+    int n,s=0,q,r,rem;
     scanf("%d",&n);
     q=n;
     while(q!=0)
     {
         r=q%10;
         s=s*10+r;
-        q/=10;
+        q=q/10;
     }
     if(s==n)
     {
-        printf("True");
+        printf("Palindrome");
     }
     else
     {
-        printf("False");
+        printf("Not Palindrome");
     }
 }
